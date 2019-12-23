8b56f7226a748793e5ed2c24dfe52f864f440c4e
paruhurimaniteja <59167267+paruhurimaniteja@users.noreply.github.com>
2019-12-23 22:48:05 +0530

    Add files via upload

---
+++ sorting.java
@@ -0,0 +1,40 @@
+package epam;
+import java.util.Scanner;
+
+public class sorting {
+	private static Scanner sc;
+
+	public static void main (String args[]) {
+		
+	
+	int i,j,temp;
+sc = new Scanner(System.in);
+     System.out.println("enter the array size");
+	int n =sc.nextInt();
+	int arr[]=new int[n];
+			System.out.println("enter the arry elements");
+	for(i=0;i<n;i++)
+	{
+		arr[i]=sc.nextInt();
+		
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(arr[i]>arr[j])
+			{
+				temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+		}
+		
+		
+	}
+		System.out.println(arr[i]);
+			
+  }
+	
+}
+	
+}
